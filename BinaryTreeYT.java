58dab7ff3fb3a0360dd1d15b05000e1ba5c52317
Abhishek Ghosh <abhi.ghosh3108@gmail.com>
2023-01-24 11:21:20 +0530

    changes made

--- BinaryTreeYT.java
+++ BinaryTreeYT.java
@@ -6,7 +6,7 @@ public class BinaryTreeYT{
         Node left;
         Node right;
         Node(int data){
-            this.data=data;
+            data=data;
             this.left=null;
             this.right = null;
         }
